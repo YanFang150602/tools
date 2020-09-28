c0ef108e65a54b538847c10a8a023fc31c7c07d3
颜泽芳 <838641570@qq.com>
2020-09-28 10:24:02 +0800

    Update webpack.config.js

--- projects/EchartsDemo/build/webpack.config.js
+++ projects/EchartsDemo/build/webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
     mode: 'development',
     entry: {
@@ -16,5 +20,22 @@ module.exports = {
                 loader: 'babel-loader'
             }
         ]
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            // webpack --watch时，不要移除index.html
+            cleanStaleWebpackAssets: false
+        }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: '../dist',
+        port: '8081',
+        open: true, // 自动打开浏览器
+        hot: true,
+        hotOnly: true
     }
 }
