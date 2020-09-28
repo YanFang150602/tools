e22a268716fd1b0781edcf55776175049f221e78
颜泽芳 <838641570@qq.com>
2020-09-28 10:22:45 +0800

    Update main.js

--- projects/EchartsDemo/src/main.js
+++ projects/EchartsDemo/src/main.js
@@ -1,5 +1,13 @@
 import echarts from 'echarts';
-import {bar} from './js/bar';
+import 'echarts/map/js/china';
+import 'echarts/map/js/province/beijing';
+import {bar, bards, grid} from './js/bar';
+import { basepie, emphasis, customcolor, tt } from './js/pie';
+import { china, beijing } from './js/map';
 
-var myChart = echarts.init(document.getElementById('root'));
-myChart.setOption(bar);
+var myChart = echarts.init(document.getElementById('bar'), 'light');
+myChart.setOption(grid);
+myChart = echarts.init(document.getElementById('pie'), 'dark');
+myChart.setOption(tt);
+myChart = echarts.init(document.getElementById('map'));
+myChart.setOption(beijing);
