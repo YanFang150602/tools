5c7adc09372281afb7c885bce22a2e0bcaa81ebf
颜泽芳 <838641570@qq.com>
2020-09-29 13:56:31 +0800

    Update index.tsx

--- projects/TSWebpackTemplate/src/index.tsx
+++ projects/TSWebpackTemplate/src/index.tsx
@@ -1,16 +1,4 @@
-class Student {
-    private name:string;
-    private age:number;
-
-    constructor(name:string, age:number) {
-        this.name = name;
-        this.age = age;
-    }
-
-    doHomework(): void {
-        console.log(`${this.name}开始做作业！`);
-    }
-}
+import Student from './components/student';
 
 const me = new Student('天天', 10);
 me.doHomework();
