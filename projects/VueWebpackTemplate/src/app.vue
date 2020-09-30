10c85386ec484ecde95a07116c89887b9fb1e11d
颜泽芳 <838641570@qq.com>
2020-09-30 09:15:45 +0800

    Create app.vue

---
+++ projects/VueWebpackTemplate/src/app.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <input type="text" :value="inputData"/>
+        <input type="button" value="提交" />
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                inputData:'我是测试哦'
+            }
+        }
+    }
+</script>
