d9fce08c5dfee892e7879a7d9c6e936aaab1f60d
颜泽芳 <838641570@qq.com>
2020-11-09 10:22:26 +0800

    Update bar.js

--- 97_projects/EchartsDemo/src/js/bar.js
+++ 97_projects/EchartsDemo/src/js/bar.js
@@ -16,3 +16,59 @@ export const bar = {
         data: [5, 20, 36, 10, 10, 20]
     }]
 };
+
+export const bards = {
+    legend: {},
+    tooltip: {},
+    dataset: {
+        // 提供一份数据。
+        source: [
+            ['product', '2015', '2016', '2017'],
+            ['Matcha Latte', 43.3, 85.8, 93.7],
+            ['Milk Tea', 83.1, 73.4, 55.1],
+            ['Cheese Cocoa', 86.4, 65.2, 82.5],
+            ['Walnut Brownie', 72.4, 53.9, 39.1]
+        ]
+    },
+    // 声明一个 X 轴，类目轴（category）。默认情况下，类目轴对应到 dataset 第一列。
+    xAxis: {type: 'category'},
+    // 声明一个 Y 轴，数值轴。
+    yAxis: {},
+    // 声明多个 bar 系列，默认情况下，每个系列会自动对应到 dataset 的每一列。
+    series: [
+        {type: 'bar'},
+        {type: 'bar'},
+        {type: 'bar'}
+    ]
+}
+
+export const grid = {
+    dataset: {
+        source: [
+            ['score', 'amount', 'product'],
+            [89.3, 58212, 'Matcha Latte'],
+            [57.1, 78254, 'Milk Tea'],
+            [74.4, 41032, 'Cheese Cocoa'],
+            [50.1, 12755, 'Cheese Brownie'],
+            [89.7, 20145, 'Matcha Cocoa'],
+            [68.1, 79146, 'Tea'],
+            [19.6, 91852, 'Orange Juice'],
+            [10.6, 101852, 'Lemon Juice'],
+            [32.7, 20112, 'Walnut Brownie']
+        ]
+    },
+    grid: {containLabel: true},
+    xAxis: {},
+    yAxis: {type: 'category'},
+    series: [
+        {
+            type: 'bar',
+            encode: {
+                // 将 "amount" 列映射到 X 轴。
+                x: 'amount',
+                // 将 "product" 列映射到 Y 轴。
+                y: 'product'
+            }
+        }
+    ]
+}
